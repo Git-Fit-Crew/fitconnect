c6b1d91595e6923a0244832e61b7b03883d029c2
Adam Bishop <adam.michael.bishop@gmail.com>
2023-04-10 10:47:15 -0500

    Added findByUsername method

--- src/main/java/com/codeup/gitfitcrew/fitconnect/repositories/UserRepository.java
+++ src/main/java/com/codeup/gitfitcrew/fitconnect/repositories/UserRepository.java
@@ -4,5 +4,5 @@ import com.codeup.gitfitcrew.fitconnect.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    User findByUsername(String username);
 }
