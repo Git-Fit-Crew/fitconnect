97f1f5edee9540b7694fc4fe8b5c2130667a3b4d
Adam Bishop <adam.michael.bishop@gmail.com>
2023-04-10 16:19:28 -0500

    Added "/messages/user" mapping for talkJs javascript

--- src/main/java/com/codeup/gitfitcrew/fitconnect/controllers/MessagesController.java
+++ src/main/java/com/codeup/gitfitcrew/fitconnect/controllers/MessagesController.java
@@ -1,8 +1,12 @@
 package com.codeup.gitfitcrew.fitconnect.controllers;
 
+import com.codeup.gitfitcrew.fitconnect.models.User;
+import com.google.gson.Gson;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -12,4 +16,13 @@ public class MessagesController {
 
         return "messages";
     }
+
+    @GetMapping(value = "/messages/user", produces = "application/json")
+    @ResponseBody
+    public String getLoggedInUser() {
+        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Gson gson = new Gson();
+        return gson.toJson(loggedInUser);
+    }
+
 }
