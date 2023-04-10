9aeaacee2c989d6531f5609d4bef9d7787965906
Adam Bishop <adam.michael.bishop@gmail.com>
2023-04-10 10:47:41 -0500

    Added security configuration

---
+++ src/main/java/com/codeup/gitfitcrew/fitconnect/config/SecurityConfiguration.java
@@ -0,0 +1,66 @@
+package com.codeup.gitfitcrew.fitconnect.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    private final UserDetailsLoader usersLoader;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                /* Login configuration */
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/profile")
+                .permitAll()
+                /* Logout configuration */
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                /* Pages that require authentication */
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers(
+                        "/friends",
+                        "/messages",
+                        "/search",
+                        "/profile"
+                )
+                .authenticated()
+                /* Pages that can be viewed without having to log in */
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers(
+                        "/",
+                        "/register",
+                        "/css/**",
+                        "/js/**")
+                .permitAll()
+
+        ;
+        return http.build();
+    }
+
+}
