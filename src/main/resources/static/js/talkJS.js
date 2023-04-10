85a60d6f6de721a2072794b3e4786322dccfee9b
Adam Bishop <adam.michael.bishop@gmail.com>
2023-04-10 16:20:44 -0500

    Working on friend messaging functionality

--- src/main/resources/static/js/talkJS.js
+++ src/main/resources/static/js/talkJS.js
@@ -1,29 +1,31 @@
-Talk.ready.then(function () {
-    var me = new Talk.User({
-        id: '123456',
-        name: 'Alice',
-        email: 'alice@example.com',
-        photoUrl: 'https://talkjs.com/images/avatar-1.jpg',
-        welcomeMessage: 'Hey there! How are you? :-)',
-    });
-    window.talkSession = new Talk.Session({
-        appId: 'sk_test_w8c1Hya7YU6NCHdxcm4iB05PEfJsdkaY',
-        me: me,
-    });
-    var other = new Talk.User({
-        id: '654321',
-        name: 'Sebastian',
-        email: 'Sebastian@example.com',
-        photoUrl: 'https://talkjs.com/images/avatar-5.jpg',
-        welcomeMessage: 'Meowdy pawtner!',
-    });
-
-    var conversation = talkSession.getOrCreateConversation(
-        Talk.oneOnOneId(me, other)
-    );
-    conversation.setParticipant(me);
-    conversation.setParticipant(other);
-
-    var inbox = talkSession.createInbox({ selected: conversation });
-    inbox.mount(document.getElementById('talkjs-container'));
+await Talk.ready;
+const appId = await fetch("/keys").then(res => res.json()).then(res => res.talkJsAppId);
+const loggedInUser = await fetch("/messages/user").then(res => res.json())
+const me = new Talk.User({
+	id: loggedInUser.id,
+	name: loggedInUser.username,
+	email: loggedInUser.email,
+	photoUrl: 'https://talkjs.com/images/avatar-1.jpg',
+	welcomeMessage: 'Hey there! How are you? :-)',
 });
+const session = new Talk.Session({
+	appId: appId,
+	me: me,
+});
+const other = new Talk.User({
+	id: '22',
+	name: 'Tigg',
+	email: 'Sebastian@example.com',
+	photoUrl: 'https://talkjs.com/images/avatar-5.jpg',
+	welcomeMessage: 'Hey, how can I help?',
+});
+
+const conversation = session.getOrCreateConversation(
+	Talk.oneOnOneId(me, other)
+);
+conversation.setParticipant(me);
+conversation.setParticipant(other);
+
+const inbox = session.createInbox();
+inbox.select(conversation);
+inbox.mount(document.getElementById('talkjs-container'));
