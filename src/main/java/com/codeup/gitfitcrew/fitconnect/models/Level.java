b69634472369039fe4578301ccd4260b48268466
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-04-10 11:06:54 -0500

    made experience level an enum

--- src/main/java/com/codeup/gitfitcrew/fitconnect/models/Level.java
+++ src/main/java/com/codeup/gitfitcrew/fitconnect/models/Level.java
@@ -1,23 +1,5 @@
 package com.codeup.gitfitcrew.fitconnect.models;
 
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.util.List;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@ToString
-@Entity
-@Table(name="experience_level")
-public class Level {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-
-    @Column(length=255, nullable = true)
-    private String name;
+public enum Level {
+    BEGINNER, INTERMEDIATE, ADVANCED, EXPERT
 }
