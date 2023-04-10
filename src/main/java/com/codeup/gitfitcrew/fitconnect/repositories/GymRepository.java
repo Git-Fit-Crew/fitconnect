cae6255318956097aa7439da851ab655b175483f
Misael Cuadrado <104469677+nelsonymarie@users.noreply.github.com>
2023-04-10 16:58:06 -0500

    update api

--- src/main/java/com/codeup/gitfitcrew/fitconnect/repositories/GymRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codeup.gitfitcrew.fitconnect.repositories;
-
-import com.codeup.gitfitcrew.fitconnect.models.Gym;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GymRepository extends JpaRepository<Gym, Long> {
-}
