563a9a33e53f10db7c1398605f3509f490674cd0
Adam Bishop <adam.michael.bishop@gmail.com>
2023-04-10 11:13:35 -0500

    Added user details loader import

--- src/main/java/com/codeup/gitfitcrew/fitconnect/config/SecurityConfiguration.java
+++ src/main/java/com/codeup/gitfitcrew/fitconnect/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.codeup.gitfitcrew.fitconnect.config;
 
+import com.codeup.gitfitcrew.fitconnect.services.UserDetailsLoader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
