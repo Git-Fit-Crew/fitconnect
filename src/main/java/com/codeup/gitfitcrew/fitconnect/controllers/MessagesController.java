91be2dc568ac14df58e060e254c4ce54fb94ae09
Adam Bishop <adam.michael.bishop@gmail.com>
2023-04-11 16:18:28 -0500

    Removed getLoggedInUser method and moved it to UserController.java. Added friends list to model to pass on to the messages view.

--- src/main/java/com/codeup/gitfitcrew/fitconnect/controllers/MessagesController.java
+++ src/main/java/com/codeup/gitfitcrew/fitconnect/controllers/MessagesController.java
@@ -1,28 +1,22 @@
 package com.codeup.gitfitcrew.fitconnect.controllers;
 
 import com.codeup.gitfitcrew.fitconnect.models.User;
-import com.google.gson.Gson;
-import org.springframework.security.core.context.SecurityContextHolder;
+import com.codeup.gitfitcrew.fitconnect.services.FriendService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
 
+@RequiredArgsConstructor
 @Controller
 public class MessagesController {
+    private final FriendService friendService;
     @GetMapping("/messages")
     public String showMessagesPage(Model model){
-
+        List<User> friends = friendService.getFriends();
+        model.addAttribute("friends", friends);
         return "messages";
     }
-
-    @GetMapping(value = "/messages/user", produces = "application/json")
-    @ResponseBody
-    public String getLoggedInUser() {
-        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        Gson gson = new Gson();
-        return gson.toJson(loggedInUser);
-    }
-
 }
