f810d98170cf1ee47271f2fa9ea1e5996d7d5942
Christina Antonuccio <antonuccio.christina@gmail.com>
2023-04-12 12:09:58 -0500

    friend fixes

--- src/main/java/com/codeup/gitfitcrew/fitconnect/config/SecurityConfiguration.java
+++ src/main/java/com/codeup/gitfitcrew/fitconnect/config/SecurityConfiguration.java
@@ -1,18 +1,14 @@
 package com.codeup.gitfitcrew.fitconnect.config;
 
 import com.codeup.gitfitcrew.fitconnect.models.User;
-import com.codeup.gitfitcrew.fitconnect.models.UserDto;
-import com.codeup.gitfitcrew.fitconnect.repositories.UserRepository;
 import com.codeup.gitfitcrew.fitconnect.services.UserDetailsLoader;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
