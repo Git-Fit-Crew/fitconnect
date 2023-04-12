a9dd15e65e6d35e575c7a743a93921013eaffcff
Adam Bishop <adam.michael.bishop@gmail.com>
2023-04-12 10:25:03 -0500

    Passed api key to search page view

--- src/main/java/com/codeup/gitfitcrew/fitconnect/controllers/SearchController.java
+++ src/main/java/com/codeup/gitfitcrew/fitconnect/controllers/SearchController.java
@@ -1,14 +1,18 @@
 package com.codeup.gitfitcrew.fitconnect.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SearchController {
-    @GetMapping("/search")
-    public String showSearchPage(){
+    @Value("${google-maps-api-key}")
+    private String googleMapsApiKey;
 
+    @GetMapping("/search")
+    public String showSearchPage(Model model){
+        model.addAttribute("apiKey", googleMapsApiKey);
         return "search";
     }
 }
