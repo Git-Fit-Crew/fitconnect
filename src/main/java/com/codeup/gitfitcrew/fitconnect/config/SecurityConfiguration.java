50724f39509e244816204df9eb29c4adf2ce38e3
Misael Cuadrado <104469677+nelsonymarie@users.noreply.github.com>
2023-04-13 15:35:04 -0500

    update api

--- src/main/java/com/codeup/gitfitcrew/fitconnect/config/SecurityConfiguration.java
+++ src/main/java/com/codeup/gitfitcrew/fitconnect/config/SecurityConfiguration.java
@@ -75,7 +75,7 @@ public class SecurityConfiguration {
                         "/css/**",
                         "/js/**",
                         "/error",
-                "/gyms")
+                        "/gyms")
                 .permitAll()
 
         ;
