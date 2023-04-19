8f116882656d7bfdc36ef246e55539a22397de4b
Adam Bishop <adam.michael.bishop@gmail.com>
2023-04-19 14:23:27 -0500

    Added findAllByType method

--- src/main/java/com/codeup/gitfitcrew/fitconnect/repositories/PreferencesRepository.java
+++ src/main/java/com/codeup/gitfitcrew/fitconnect/repositories/PreferencesRepository.java
@@ -4,8 +4,10 @@ import com.codeup.gitfitcrew.fitconnect.models.Preferences;
 import com.codeup.gitfitcrew.fitconnect.models.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PreferencesRepository extends JpaRepository<Preferences, Long> {
     Optional<Preferences> findByNameAndType(String name, Type type);
+    List<Preferences> findAllByType(Type type);
 }
