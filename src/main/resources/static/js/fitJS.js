791edd3603c0edf6dcd4906d5a499a815dc0ecc2
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-04-25 14:26:47 -0500

    added tooltips to friends page

--- src/main/resources/static/js/fitJS.js
+++ src/main/resources/static/js/fitJS.js
@@ -43,3 +43,8 @@ daysOfMonth.forEach(
         calendarEl.appendChild(monthEl);
     }
 );
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
