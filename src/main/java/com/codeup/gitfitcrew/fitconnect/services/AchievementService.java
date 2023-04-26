b2d48e21f726e614ed421948f19f1b7d5079fa26
Adam Bishop <adam.michael.bishop@gmail.com>
2023-04-26 11:06:35 -0500

    Added logic for workouts achievements

--- src/main/java/com/codeup/gitfitcrew/fitconnect/services/AchievementService.java
+++ src/main/java/com/codeup/gitfitcrew/fitconnect/services/AchievementService.java
@@ -3,6 +3,7 @@ package com.codeup.gitfitcrew.fitconnect.services;
 import com.codeup.gitfitcrew.fitconnect.models.Achievements;
 import com.codeup.gitfitcrew.fitconnect.models.User;
 import com.codeup.gitfitcrew.fitconnect.repositories.AchievementRepository;
+import com.codeup.gitfitcrew.fitconnect.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +13,13 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Service
 public class AchievementService {
+    public static final int FIT_WORK_NOVICE_WORKOUTS = 1;
+    public static final int FIT_WORK_APPRENTICE_WORKOUTS = 10;
+    public static final int FIT_WORK_JOURNEYMAN_WORKOUTS = 25;
+    public static final int FIT_WORK_PRO_WORKOUTS = 50;
     private final AchievementRepository achievementDao;
     private final FriendService friendService;
+    private final UserRepository userDao;
 
     public void checkAndSetAchievements(User user) {
         ArrayList<Long> achievementIds = user.getAchievements().stream()
@@ -25,7 +31,19 @@ public class AchievementService {
         if (!achievementIds.contains(2L) && hasAchievedFitConnected(user)) {
             achievementDao.findById(2L).ifPresent(user.getAchievements()::add);
         }
-        System.out.println("user = " + user);
+        if (!achievementIds.contains(3L) && workoutsLogged(user) >= FIT_WORK_NOVICE_WORKOUTS) {
+            achievementDao.findById(3L).ifPresent(user.getAchievements()::add);
+        }
+        if (!achievementIds.contains(4L) && workoutsLogged(user) >= FIT_WORK_APPRENTICE_WORKOUTS) {
+            achievementDao.findById(4L).ifPresent(user.getAchievements()::add);
+        }
+        if (!achievementIds.contains(5L) && workoutsLogged(user) >= FIT_WORK_JOURNEYMAN_WORKOUTS) {
+            achievementDao.findById(5L).ifPresent(user.getAchievements()::add);
+        }
+        if (!achievementIds.contains(6L) && workoutsLogged(user) >= FIT_WORK_PRO_WORKOUTS) {
+            achievementDao.findById(6L).ifPresent(user.getAchievements()::add);
+        }
+        userDao.save(user);
     }
 
     private boolean hasAchievedFitProfile(User user) {
@@ -45,7 +63,7 @@ public class AchievementService {
         return friendService.hasFriends(user);
     }
 
-    private void hasAchievedFitWork(User user) {
-
+    private int workoutsLogged(User user) {
+        return user.getWorkouts().size();
     }
 }
