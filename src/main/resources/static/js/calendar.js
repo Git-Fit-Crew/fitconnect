4338c40ea9b107ce25c532e6cedc7f5c00d1ea82
Adam Bishop <adam.michael.bishop@gmail.com>
2023-04-28 12:47:15 -0500

    Edited calendar size and date display. Added a spinner for when the calendar is loading

--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -3,7 +3,17 @@ const cal = new CalHeatMap();
 const userId = document.querySelector("#user-id").value;
 let today = new Date();
 
+function displayLoadingSpinner(shouldDisplay) {
+	const spinner = document.getElementById("calendar-spinner");
+	if (shouldDisplay) {
+		spinner.classList.remove("d-none");
+	} else {
+		spinner.classList.add("d-none");
+	}
+}
+
 async function getUserWorkouts() {
+	displayLoadingSpinner(true);
 	const workouts = await fetch("/workouts/" + userId).then(res => res.json());
 	let data = {};
 	workouts.forEach(function (e) {
@@ -13,12 +23,13 @@ async function getUserWorkouts() {
 }
 
 getUserWorkouts().then(function (data){
+	displayLoadingSpinner(false);
 	cal.init({
 		start: new Date(today.getFullYear(), today.getMonth() - 4, today.getDate()),
 		domain: 'month',
 		subDomain: 'day',
-		cellSize: 10,
-		subDomainTextFormat: '',
+		cellSize: 18,
+		subDomainTextFormat: '%d',
 		range: 5,
 		displayLegend: false,
 		cellRadius: 10,
