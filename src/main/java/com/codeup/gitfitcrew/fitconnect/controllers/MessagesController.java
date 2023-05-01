7d8c34e9d1156bd36a237d305bb079cff8b4f9ca
Adam Bishop <adam.michael.bishop@gmail.com>
2023-04-30 19:31:37 -0500

    Refactored getting the logged-in user in both get mapping methods. Added TalkJsService method for updating conversation access based on friend status

--- src/main/java/com/codeup/gitfitcrew/fitconnect/controllers/MessagesController.java
+++ src/main/java/com/codeup/gitfitcrew/fitconnect/controllers/MessagesController.java
@@ -2,7 +2,6 @@ package com.codeup.gitfitcrew.fitconnect.controllers;
 
 import com.codeup.gitfitcrew.fitconnect.models.User;
 import com.codeup.gitfitcrew.fitconnect.repositories.UserRepository;
-import com.codeup.gitfitcrew.fitconnect.services.FriendService;
 import com.codeup.gitfitcrew.fitconnect.services.TalkJsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,38 +9,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class MessagesController {
-    private final FriendService friendService;
     private final UserRepository userDao;
     private final TalkJsService talkJsService;
 
     @GetMapping("/messages")
-    public String showMessagesPage(Model model){
-        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        User currentUser = userDao.getUserById(user.getId());
-        if (currentUser == null) {
-            return "login";
-        }
-        updateConversations(String.valueOf(currentUser.getId()));
-//        List<User> friends = friendService.getFriends();
-//        model.addAttribute("friends", friends);
+    public String showMessagesPage(){
+        User currentUser = getLoggedInUser();
+        if (currentUser == null) return "login";
+        talkJsService.setAccessForAllConversations(currentUser);
         return "messages";
     }
 
     @GetMapping("/messages/{id}")
     public String showMessagesWithFriend(@PathVariable long id, Model model){
         User friend = userDao.getUserById(id);
+        User currentUser = getLoggedInUser();
+        if (currentUser == null) return "login";
+        talkJsService.setAccessForAllConversations(currentUser);
         model.addAttribute("friendToChat", friend);
         return "messages";
     }
 
-    private void updateConversations(String id) {
-        talkJsService.updateConversationAccessForUser(id);
+    private User getLoggedInUser() {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return userDao.getUserById(user.getId());
     }
 }
